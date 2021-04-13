ef7176c8cc278903a2398646f084d4ccd4062f87
JP Meyer <jp@jpmeyer.dev>
2021-04-13 17:42:19 +0200

    Added sidebar to admin dashboard

--- src/components/dashboard-container/DashboardContainer.tsx
+++ src/components/dashboard-container/DashboardContainer.tsx
@@ -1,34 +1,123 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import React from "react";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {logout, selectAuth} from "../../features/auth/authSlice";
+import {Button, Container, ListGroup, Nav, Navbar} from "react-bootstrap";
+import styled from "styled-components";
 
 export interface DashboardContainerProps {
     children: any;
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Sidebar = styled.div`
+  width: 250px;
+  position: fixed;
+  top: 0;
+  left: -250px;
+  height: 100vh;
+  z-index: 999;
+  background: #e9ecef;
+  transition: all 0.3s;
+
+  &.active {
+    left: 0;
+  }
+`;
+
+const Content = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  transition: all 0.3s;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const SidebarOverlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  transition: 0.3s;
+  
+  &.active {
+    background: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    z-index: 998;
+  }
+`;
+
 const DashboardContainer: React.FC<DashboardContainerProps> = ({children}) => {
     const auth = useAppSelector(selectAuth);
     const dispatch = useAppDispatch();
 
+    const location = useLocation()
+    const [sidebarActive, setSidebarActive] = React.useState(false);
+    const [sidebarClass, setSidebarClass] = React.useState('');
+    const [contentClass, setContentClass] = React.useState('active');
+
+    React.useEffect(() => {
+        setSidebarClass(sidebarActive ? 'active' : '');
+        setContentClass(sidebarActive ? '' : 'active');
+    }, [sidebarActive]);
+
+
     return (
-        <div>
-            DASH
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/dashboard">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">Users</Link>
-                    </li>
-                    {
-                        auth.token && <button onClick={() => dispatch(logout())}>Logout</button>
-                    }
-                </ul>
-            </nav>
-            {children}
-        </div>
+        <Wrapper className="admin-wrapper">
+            <SidebarOverlay className={sidebarClass} onClick={() => setSidebarActive(false)}>
+                <Sidebar id="sidebar" className={sidebarClass}>
+                    <div className="sidebar-header p-3">
+                        <h3>Admin Dashboard</h3>
+                    </div>
+                    <ListGroup>
+                        <ListGroup.Item action to="/dashboard" as={Link} active={location.pathname === '/dashboard'}>
+                            Home
+                        </ListGroup.Item>
+                        <ListGroup.Item action to="/users" as={Link} active={location.pathname === '/users'}>
+                            Users
+                        </ListGroup.Item>
+                        <ListGroup.Item action>
+                            Link 3
+                        </ListGroup.Item>
+                        <ListGroup.Item action>
+                            Link 4
+                        </ListGroup.Item>
+                        <ListGroup.Item action>
+                            Link 5
+                        </ListGroup.Item>
+                        <ListGroup.Item action>
+                            Link 6
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Sidebar>
+            </SidebarOverlay>
+            <Content id="content">
+                {/*<button onClick={() => setSidebarActive(!sidebarActive)}>Nav bar</button>*/}
+                <Navbar bg="light" expand="lg">
+                    <Nav className="mr-auto">
+                        <Button
+                            variant={"outline-info"}
+
+                            onClick={() => setSidebarActive(!sidebarActive)}>
+                            <span className="navbar-toggler-icon"></span>
+                        </Button>
+                    </Nav>
+                    <Navbar.Text>
+                        Welcome, user
+                    </Navbar.Text>
+                </Navbar>
+                <Container fluid className={"p-3"}>
+                    {children}
+                </Container>
+            </Content>
+        </Wrapper>
     )
 }
 
