8c1e4173f6812f4f0ca57a58aec4d6454562f241
JP Meyer <jp@jpmeyer.dev>
2021-04-22 08:25:07 +0200

    Made data table smarter
    Data tables can now perform their own API calls to endpoints for generic data. They will display columns and actions based on configuration rather than knowledge of how a model looks. Data tables can also now perform their own pagination, filtering and delete actions. It might be worth considering an abstract implementation of DataModels that can dispatch their own actions for management and such.

---
+++ src/models/DataModel.ts
@@ -0,0 +1,11 @@
+// Abstract class used for data models that can be displayed in tables.
+
+export abstract class DataModel {
+    // TODO: Add abstract methods:
+    /**
+     * fetchList
+     * fetchOne
+     * delete
+     * save
+     */
+}
